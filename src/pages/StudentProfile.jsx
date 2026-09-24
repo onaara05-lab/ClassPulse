8e64caf70ce016d1ed1f886004bbcd4adf0740cb
Onaara23-lab <amusanonaara23@gmail.com>
2026-09-24 15:09:57 +0100

    fixed (studentSidebar) missing import in student profile

--- src/pages/StudentProfile.jsx
+++ src/pages/StudentProfile.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import StudentSidebar from "../components/student/StudentSidebar";
+import StudentSidebar from "../components/Student/StudentSidebar";
 
 const studentProfile = {
   name: "Chukwuemeka Eze",
